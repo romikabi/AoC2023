e0d96d390011bd131011291de403b36d1071ec52
Roman Abuziarov <abuzyarovroman@gmail.com>
2023-12-08 11:22:15 +0100

    day 08 part 2

--- Tests/Day08.swift
+++ Tests/Day08.swift
@@ -30,4 +30,21 @@ final class Day08Tests: XCTestCase {
     let challenge = Day08(data: data)
     XCTAssertEqual(String(describing: challenge.part1()), "6")
   }
+
+  func testPart2() {
+    let data = """
+      LR
+
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
+      """
+    let challenge = Day08(data: data)
+    XCTAssertEqual(String(describing: challenge.part2()), "6")
+  }
 }

f2a05418616cb0f8f0c3ae0bd75901b0263f1dc2
Roman Abuziarov <abuzyarovroman@gmail.com>
2023-12-09 10:21:16 +0100

    day 09 part 2

--- Sources/Day09.swift
+++ Sources/Day09.swift
@@ -16,5 +16,24 @@ struct Day09: AdventDay {
     .reduce(0, +)
   }
 
+  func part2() -> Any {
+    data.split(separator: "\n").map { line in
+      var nums = line.split(separator: " ").map(String.init).compactMap(Int.init)
+      var firsts = [nums.first ?? 0]
+      while !nums.allSatisfy({ $0 == 0 }) {
+        nums = nums.adjacentPairs().map { $1 - $0 }
+        firsts.append(nums.first ?? 0)
+      }
+      return firsts.reversed().reduce(0) { $1 - $0 }
+    }
+    .reduce(0, +)
+  }
+
   private let data: String
 }
+
+// 10 3 0 2 0
+// 0
+
+// 10
+// 5
--- Tests/Day09.swift
+++ Tests/Day09.swift
@@ -3,13 +3,19 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day09Tests: XCTestCase {
+  private let data = """
+    0 3 6 9 12 15
+    1 3 6 10 15 21
+    10 13 16 21 30 45
+    """
+
   func testPart1() {
-    let data = """
-      0 3 6 9 12 15
-      1 3 6 10 15 21
-      10 13 16 21 30 45
-      """
     let challenge = Day09(data: data)
     XCTAssertEqual(String(describing: challenge.part1()), "114")
+  }
+
+  func testPart2() {
+    let challenge = Day09(data: data)
+    XCTAssertEqual(String(describing: challenge.part2()), "2")
   }
 }

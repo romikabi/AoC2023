af049461cb61c073bf857c963594c3f9692e3189
Roman Abuziarov <abuzyarovroman@gmail.com>
2023-12-10 12:23:01 +0100

    day 10 part 2

--- Sources/Day10.swift
+++ Sources/Day10.swift
@@ -5,13 +5,7 @@ struct Day10: AdventDay {
 
   func part1() -> Any {
     let pipes = data.split(separator: "\n").map { $0.map { $0 } }
-    let start = pipes.lazy.indices.flatMap { i in
-      pipes[i].lazy.indices.map { j in
-        Index(i: i, j: j)
-      }
-    }.first { index in
-      pipes[index.i][index.j] == "S"
-    }!
+    let start = startIndex(in: pipes)
 
     var steps = 0
     var last = start
@@ -24,6 +18,55 @@ struct Day10: AdventDay {
     } while pipe != start
 
     return steps / 2
+  }
+
+  func part2() -> Any {
+    let pipes = data.split(separator: "\n").map { $0.map { $0 } }
+    let start = startIndex(in: pipes)
+
+    enum Tile {
+      case ground
+      case boundary
+      case pipe
+    }
+
+    var pureGrid = pipes.map { row in
+      Array(repeating: "." as Character, count: row.count)
+    }
+
+    var last = start
+    var pipe = start
+    repeat {
+      let t = pipe
+      pipe = connectedPipes(around: pipe, in: pipes).first { $0 != last }!
+      last = t
+      setTile(tile(at: pipe, in: pipes)!, at: pipe, in: &pureGrid)
+    } while pipe != start
+
+    // specific to the input
+    setTile("J", at: start, in: &pureGrid)
+
+    return pureGrid.reduce(into: 0) { result, row in
+      var inside = false
+      var e: Expectation?
+      for tile in row {
+        switch tile {
+        case "." where inside:
+          result += 1
+        case "|":
+          inside.toggle()
+        case let x where x == e?.toggle:
+          inside.toggle()
+          e = nil
+        case let x where x == e?.keep:
+          e = nil
+        default:
+          if let exp = expectation(after: tile) {
+            e = exp
+          }
+        }
+      }
+    }
   }
 
   private let data: String
@@ -47,6 +90,8 @@ private enum Delta: Hashable {
   case i(Int)
   case j(Int)
 }
+
+private typealias Expectation = (toggle: Character, keep: Character)
 
 private func connectedPipes(around index: Index, in pipes: [[Character]]) -> [Index] {
   guard let tile = tile(at: index, in: pipes) else { return [] }
@@ -76,9 +121,34 @@ private func connections(tile: Character) -> Set<Delta> {
   }
 }
 
-private func tile(at index: Index, in pipes: [[Character]]) -> Character? {
+private func tile<T>(at index: Index, in pipes: [[T]]) -> T? {
   guard pipes.indices.contains(index.i), pipes[index.i].indices.contains(index.j) else {
     return nil
   }
   return pipes[index.i][index.j]
+}
+
+private func setTile<T>(_ tile: T, at index: Index, in pipes: inout [[T]]) {
+  guard pipes.indices.contains(index.i), pipes[index.i].indices.contains(index.j) else {
+    return
+  }
+  pipes[index.i][index.j] = tile
+}
+
+private func startIndex(in pipes: [[Character]]) -> Index {
+  pipes.lazy.indices.flatMap { i in
+    pipes[i].lazy.indices.map { j in
+      Index(i: i, j: j)
+    }
+  }.first { index in
+    pipes[index.i][index.j] == "S"
+  }!
+}
+
+private func expectation(after tile: Character) -> Expectation? {
+  switch tile {
+  case "L": return ("7", "J")
+  case "F": return ("J", "7")
+  default: return nil
+  }
 }
